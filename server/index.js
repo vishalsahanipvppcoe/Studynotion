8327c0f4ad9219ed76c3848f404f1cc36776f115
Vishal Sahani <vu4f2324152@pvppcoe.ac.in>
2026-06-02 22:39:23 +0530

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://m-three-red.vercel.app" // ✅ ADDED (IMPORTANT)
+      "https://studynotion-liard-zeta.vercel.app",
     ],
     credentials: true,
   })
