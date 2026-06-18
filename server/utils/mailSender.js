9e8a411c4daa75d2d387f094ce73ef14f50ed2a5
Vishal Sahani <vu4f2324152@pvppcoe.ac.in>
2026-06-18 15:11:06 +0530

    Fix mailSender configuration for Render

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -3,7 +3,9 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
     try{
             let transporter = nodemailer.createTransport({
-                host:process.env.MAIL_HOST,
+                host: process.env.MAIL_HOST || "smtp.gmail.com",
+                port: 587,
+                secure: false,
                 auth:{
                     user: process.env.MAIL_USER,
                     pass: process.env.MAIL_PASS,
